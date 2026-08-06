7b06afb091930e6bfabb6cc9698de1253ebdb896
Aastha <aasthakushwaha552@gmail.com>
2026-08-06 19:47:16 +0530

    Add EvenOdd class for even/odd number check
    Added EvenOdd class to determine if a number is even or odd.

--- conditional.java
+++ conditional.java
@@ -1,3 +1,4 @@
+//greatest number in three
 public class conditional {
     public static void main(String[] args) {
         int a = 3;
@@ -12,5 +13,18 @@ public class conditional {
         else {
             System.out.println("greatest number "+c);
         }
+    }
+}
+// evenodd
+mport java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args){
+    Scanner sc= new Scanner (System.in);
+    int n= sc.nextInt();
+    if(n%2==0){
+    System.out.print("Number is Even" +n);}
+    else{
+    System.out.print("Number is odd" +n);}
     }
 }
