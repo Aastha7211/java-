dcdd427586f48f7b8114ee82d4dc7e74a24ca73c
Aasth7211 <24114cn011@glbitm.ac.in>
2026-08-09 12:46:24 +0530

    loops

--- Loops/BasicLoops.java
+++ Loops/BasicLoops.java
@@ -8,3 +8,25 @@ public class BasicLoops {
         }
     }
 }
+
+
+//public class BasicLoops {
+//    public static void main(String[] args){
+//
+//        for(int i=0;i< 5;i++ ){
+//            System.out.println(i);
+//        }
+//    }
+//}
+
+/*public class BasicLoops {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n= sc.nextInt();
+        for(int i=0;i<=n;i++ ){
+            System.out.println(i);
+        }
+    }
+}
+*/
