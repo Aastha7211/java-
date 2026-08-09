be11c3b78c618c7e2a85c3654d1c54b6d5377380
Aasth7211 <24114cn011@glbitm.ac.in>
2026-08-09 12:15:46 +0530

    loops

---
+++ Loops/BasicLoops.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class BasicLoops {
+    public static void main(String[] args){
+        int n=6;
+        for(int i=0;i< 5;i++ ){
+            System.out.println("Hello");
+        }
+    }
+}
