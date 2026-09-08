afd923f2a2c1f80a4bf42ccf0756887eb9c74436
Aasth7211 <24114cn011@glbitm.ac.in>
2026-09-08 20:10:55 +0530

    loops

---
+++ Chatgpt/module1/PrintAllElement.java
@@ -0,0 +1,23 @@
+package Chatgpt.module1;
+
+import java.util.Scanner;
+
+public class PrintAllElement {
+
+    public static  void main(String[] args){
+        Scanner sc=  new Scanner(System.in);
+        System.out.print("Enter the size if array");
+        int n= sc.nextInt();
+        int[]arr= new int[n];
+        for (int i=0;i<n;i++)
+        {
+//            System.out.println("enter the element of array");
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
